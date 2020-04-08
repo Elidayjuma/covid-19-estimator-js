1b851fda1d72dff22756ac284d74da3ca7f303a7
unknown <elidejuma@gmail.com>
2020-04-08 14:46:21 +0300

    Test for challenge 2.

--- src/estimator.js
+++ src/estimator.js
@@ -14,12 +14,12 @@ const covid19ImpactEstimator = (data) => {
   const setOfDays = Math.floor(estimateTime / 3);
   const infectionsByRequestedTime = currentlyInfected * (2 ** setOfDays);
   const serverinfectionsByRequestedTime = serverCurrentlyInfcted * (2 ** setOfDays);
-  const severeCasesByRequestedTime = (0.15 * infectionsByRequestedTime);
-  const ServeresevereCasesByRequestedTime = (0.15 * serverinfectionsByRequestedTime);
+  const severeCasesByRequestedTime = ((15 / 100) * infectionsByRequestedTime);
+  const ServeresevereCasesByRequestedTime = ((15 / 100) * serverinfectionsByRequestedTime);
   // calculate the number of beds
   const bedsAreadyOccupied = (0.65 * data.totalHospitalBeds);
-  const hospitalBedsByRequestedTime = severeCasesByRequestedTime - bedsAreadyOccupied;
-  const serverehospitalBedsByRequestedTime = ServeresevereCasesByRequestedTime - bedsAreadyOccupied;
+  const hospitalBedsByRequestedTime = bedsAreadyOccupied - severeCasesByRequestedTime;
+  const serverehospitalBedsByRequestedTime = bedsAreadyOccupied - ServeresevereCasesByRequestedTime;
 
 
   // return reponse data

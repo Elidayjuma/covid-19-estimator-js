9221073dba9964a5415a864f06aa6c07617e48ff
unknown <elidejuma@gmail.com>
2020-04-08 12:31:11 +0300

    Challenge 1 complete.

--- src/estimator.js
+++ src/estimator.js
@@ -1,3 +1,46 @@
-const covid19ImpactEstimator = (data) => data;
+const covid19ImpactEstimator = (data) => {
+  const input = data;
+  const currentlyInfected = data.reportedCases * 10;
+  const serverCurrentlyInfcted = data.reportedCases * 50;
+  // Calculate the time to elapse
+  let estimateTime;
+  if (data.periodType === 'days') {
+    estimateTime = data.timeToElapse;
+  } else if (data.periodType === 'weeks') {
+    estimateTime = data.timeToElapse * 7;
+  } else if (data.periodType === 'months') {
+    estimateTime = data.timeToElapse * 30;
+  }
+  const setOfDays = Math.floor(estimateTime / 3);
+  const infectionsByRequestedTime = currentlyInfected * (2 ** setOfDays);
+  const serverinfectionsByRequestedTime = serverCurrentlyInfcted * (2 ** setOfDays);
+  return {
+    data: input,
+    impact: {
+      currentlyInfected,
+      infectionsByRequestedTime
+    },
+    severeImpact: {
+      currentlyInfected: serverCurrentlyInfcted,
+      infectionsByRequestedTime: serverinfectionsByRequestedTime
+    }
+  };
+};
+
+
+// const data = {
+//   region: {
+//     name: 'Africa',
+//     avgAge: 19.7,
+//     avgDailyIncomeInUSD: 5,
+//     avgDailyIncomePopulation: 0.71
+//   },
+//   periodType: 'days',
+//   timeToElapse: 58,
+//   reportedCases: 674,
+//   population: 66622705,
+//   totalHospitalBeds: 1380614
+// };
+// covid19ImpactEstimator(data);
 
 export default covid19ImpactEstimator;
